c444a6afeb7702965f547383b4582653e443321b
hnimt <tongminh23996@gmail.com>
2022-06-05 14:55:03 +0700

    chore: setup nginx, serve staticfiles

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -128,8 +128,8 @@ MANAGERS = ADMINS
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = str(ROOT_DIR / 'staticfiles')
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 STATICFILES_DIRS = []
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
